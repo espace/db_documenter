a3331c7f75260b3478de012b52d74d3c8018b4da
Ahmed Yehia <ahmed.yehia311@gmail.com>
2019-07-11 13:24:35 +0200

    Make footer Configurable

--- lib/database_documenter/configuration.rb
+++ lib/database_documenter/configuration.rb
@@ -1,11 +1,12 @@
 module DatabaseDocumenter
   class Configuration
-    attr_accessor :skipped_modules, :hidden_values_columns, :database_configuration
+    attr_accessor :skipped_modules, :hidden_values_columns, :database_configuration, :footer
 
     def initialize
       @skipped_modules = []
       @hidden_values_columns = ['encrypted_password', 'current_sign_in_ip', 'remote_address', 'last_sign_in_ip']
       @database_configuration = Rails.application.config.database_configuration[Rails.env]
+      @footer = ''
     end
   end
 end
