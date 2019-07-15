cbd410d9e7deb7697da5982fb8ce38573e3f6064
Ahmed Yehia <ahmed.yehia311@gmail.com>
2019-07-15 23:50:40 +0200

    Remove test spec

--- spec/database_documenter_spec.rb
+++ spec/database_documenter_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe DatabaseDocumenter do
   it "has a version number" do
     expect(DatabaseDocumenter::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
